740da4444b6213e72e95a5089d361d56e04a8296
Atul Gopinathan <atulgopinathan@gmail.com>
2021-04-19 23:48:28 +0530

    Added VGA implementation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use::core::panic::PanicInfo;
 
 // We need to define our own panic handler as we can't use the one in
 // standard library.
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
-static HELLO: &[u8] = b"Hello New OS and Atulu!";
+//static HELLO: &[u8] = b"Hello New OS and Atulu!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    //panic!("random panic message");
+    println!("Hello World {}", "!");
 
     loop{}
 }
