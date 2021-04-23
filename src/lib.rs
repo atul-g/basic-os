b5adea374d41419ddff9a7734a61e58bdbd315f5
Atul Gopinathan <atulgopinathan@gmail.com>
2021-04-23 21:51:51 +0530

    Added a breakpoint CPU exception with the help of x86-64 crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,13 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -40,10 +42,16 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     loop {}
 }
 
+// For our exceptions of CPU
+pub fn init() {
+    interrupts::init_idt();
+}
+
 /// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
