b5e375cadb8fbbd9b1ec6cee808221f9505760af
Atul Gopinathan <atulgopinathan@gmail.com>
2021-05-05 22:34:37 +0530

    Implemented Heap Allocation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
 
 use core::panic::PanicInfo;
 
@@ -18,6 +21,13 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod memory;
+pub mod allocator;
+
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
+}
 
 pub trait Testable {
     fn run(&self) -> ();
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@
 #![test_runner(basic_rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use basic_rust_os::println;
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use basic_rust_os::memory;
-    use basic_rust_os::memory::BootInfoFrameAllocator;
-    use x86_64::{structures::paging::{Page, Translate}, VirtAddr};
-
-    //use x86_64::VirtAddr;
+    use basic_rust_os::allocator;
+    use basic_rust_os::memory::{self, BootInfoFrameAllocator};
 
     println!("Hello World{}", "!");
     basic_rust_os::init();
@@ -26,30 +27,29 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
 
-    //mapping an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+    
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
 
-    //write a string, "New!" to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
 
-    //fn stack_overflow() {
-    //    stack_overflow(); // for each recursion, the return address is pushed
-    //}
-    // uncomment line below to trigger a stack overflow
-    // stack_overflow();
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-    // provoking a deadlock and testing it works
-    //loop {
-    //    use basic_rust_os::print;
-    //    for _ in 0..10000 {}
-    //    print!("-");
-    //}
     basic_rust_os::hlt_loop();
 }
 
