5576bace480551c6bcec8bfd3efa7e73c671380f
Atul Gopinathan <atulgopinathan@gmail.com>
2021-05-05 13:02:25 +0530

    Implemented paging and page translations

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,17 @@
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 pub mod gdt;
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
+pub mod memory;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -60,8 +67,7 @@ pub fn init() {
 
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,33 @@
 
 use basic_rust_os::println;
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use basic_rust_os::memory;
+    use basic_rust_os::memory::BootInfoFrameAllocator;
+    use x86_64::{structures::paging::{Page, Translate}, VirtAddr};
+
+    //use x86_64::VirtAddr;
+
     println!("Hello World{}", "!");
-
     basic_rust_os::init();
 
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
-    use x86_64::registers::control::Cr3;
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at {:?}", level_4_page_table.start_address());
+    //mapping an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    //write a string, "New!" to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     //fn stack_overflow() {
     //    stack_overflow(); // for each recursion, the return address is pushed
